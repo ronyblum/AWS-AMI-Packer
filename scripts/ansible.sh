7c0bf25564ec8fa46a9d6537a6aafd2eea605157
Rony Blum <ronyblum@gmail.com>
2018-09-23 09:18:15 +0000

    version 0.3

---
+++ scripts/ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+sudo apt-get update
+echo "APT updated"
+sudo apt-get -y upgrade
+sudo apt-get install -y python-dev python-pip git
+sudo pip install ansible
+sudo timedatectl set-timezone Asia/Jerusalem
+sudo localectl set-locale LANG=en_US.utf8
+sudo git clone "https://github.com/ronyblum/AWS-AMI-Packer.git"
+cd AWS-AMI-Packer/ansible
+echo "Running Ansible Playbook"
+sudo ansible-playbook lamp.yml
+echo "Installation completed. Removing ansible playbook"
+rm -rf ~/AWS-AMI-Packer
